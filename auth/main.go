b89dc3a8b42985e505394a0b7f73b4dfc529f393
Jacob Ekanem <prometheus6001@gmail.com>
2024-11-10 18:47:37 -0400

    Add auth service with database setup
    Integrated an authentication service with a Postgres database. Introduced database connection and migration logic using GORM and golang-migrate. Updated `docker-compose.yml` to include a Postgres service, leveraging an SQL file for initial setup. Added IntelliJ project configuration files to `.gitignore` to prevent committing unnecessary data. Includes new dependencies in `go.mod` for managing database interactions and migrations.

--- auth/main.go
+++ auth/main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"open-property.com/auth/database"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
+	db, err := database.ConnectDB("host=auth_db user=authdb password=password dbname=authdb port=5432 sslmode=disable")
+	if err != nil {
+		log.Fatalf("failed to connect to database: %v", err)
+	}
+
+	_ = db
+
+	// Run migrations
+	database.RunMigrations("host=auth_db user=authdb password=password dbname=authdb port=5432 sslmode=disable")
+
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
